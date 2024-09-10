41fd0ab4230f9bb309304818ccff3d8d2fdbf052
Halee0090 <165649741+Halee0090@users.noreply.github.com>
2024-09-10 11:21:25 +0900

    Haeun (#14)
    * merge main screen check & image classify completed
    
    * wellness main screen check & image classify feature developed
    
    * update Dockerfile
    
    * app/api/v1/recommend.py updated
    
    * updated
    
    * dokerfile deleted
    
    * docker-compose file added, docker file deleted

--- Fastapi-backend/app/main.py
+++ Fastapi-backend/app/main.py
@@ -1,12 +1,17 @@
 # /app/main.py
 from fastapi import FastAPI
 from app.api.v1 import recommend, model
+from app.api.v1.history import router as history_router
+
 
 app = FastAPI()
 
-app.include_router(recommend.router, prefix="/recommend", tags=["Recommend"])
-app.include_router(model.router, prefix="/model", tags=["Model"])
+app.include_router(recommend.router, prefix="/api/v1/recommend", tags=["Recommend"])
+app.include_router(model.router, prefix="/api/v1/model", tags=["Model"]) 
+app.include_router(history_router, prefix="/api/v1/history")
+  
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn
+

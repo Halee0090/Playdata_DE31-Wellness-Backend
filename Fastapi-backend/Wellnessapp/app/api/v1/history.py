e4669814306c86d0be5169fec90781b966fb672e
Halee0090 <165649741+Halee0090@users.noreply.github.com>
2024-09-11 16:29:07 +0900

    docker-compose 성공하여 flutter 팀에 api 전달 완료 (#27)
    * merge main screen check & image classify completed
    
    * wellness main screen check & image classify feature developed
    
    * update Dockerfile
    
    * app/api/v1/recommend.py updated
    
    * updated
    
    * dokerfile deleted
    
    * docker-compose file added, docker file deleted
    
    * Add .env to .gitignore
    
    * Remove .env file from Git history
    
    * added env_file to docker-compose.yml
    
    * s3.py aws key updated
    
    * dockerfile, dockerfile-model added, docker-compose.yml revised
    
    * dockerfile model_api revised
    
    * dockerfile, dockerfile-model revised, model folder created, docker-compose.yml revised (2)
    
    * docker compose ready
    
    * all ficture api completed
    
    * wellness updated
    
    * rout folder dockerfile deleted
    
    * dockerfile updated
    
    * docker-compose success
    
    * README updated

--- Fastapi-backend/Wellnessapp/app/api/v1/history.py
+++ Fastapi-backend/Wellnessapp/app/api/v1/history.py
@@ -4,32 +4,29 @@ from db.session import get_db
 from db.crud import create_history
 from datetime import datetime
 from db.models import History
+from schemas.history import HistoryCreateRequest
 
 router = APIRouter()
 
 @router.post("/save_and_get")
 def save_to_history_and_get_today_history(
-    user_id: int, 
-    category_id: int, 
-    meal_type_id: int, 
-    image_url: str, 
-    date: datetime,  # 저장할 날짜를 직접 전달받음
+    history_data: HistoryCreateRequest,  # Body로 데이터를 받음
     db: Session = Depends(get_db)
 ):
     try:
         # 1. 새 기록을 데이터베이스에 저장
         new_history = create_history(
             db=db,
-            user_id=user_id,
-            category_id=category_id,
-            meal_type_id=meal_type_id,
-            image_url=image_url,
-            date=date
+            user_id=history_data.user_id,
+            category_id=history_data.category_id,
+            meal_type_id=history_data.meal_type_id,
+            image_url=history_data.image_url,
+            date=history_data.date
         )
         db.commit()  # 저장 완료
 
         # 2. 저장된 기록을 바로 반환
-        history_data = {
+        response_data = {
             "history_id": new_history.id,  # 기록 ID
             "category_id": new_history.category_id,  # 카테고리 ID
             "meal_type_id": new_history.meal_type_id,  # 식사 유형 ID
@@ -39,7 +36,7 @@ def save_to_history_and_get_today_history(
         return {
             "status": "success",
             "status_code": 201,
-            "detail": history_data,  # 저장된 기록 반환
+            "detail": response_data,  # 저장된 기록 반환
             "message": "History saved successfully"
         }
 

6b6435b9e4b9197137c301473dba7d0965e21c0c
haeunlee <gkdmsgkdms22@gmail.com>
2024-09-16 14:34:58 +0900

    updated before pull dev

--- wellness/app/database.py
+++ wellness/app/database.py
@@ -1,49 +1,25 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from fastapi import Depends
-from sqlalchemy.orm import Session, declarative_base
-from typing import Generator
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-# .env 파일 로드
+# .env 파일 로드(환경변수 설정)
 load_dotenv()
 
-# 데이터베이스 URL 설정 
+# postgresql 데이터베이스 url을 환경변수로부터 가져오기
 DATABASE_URL = os.getenv("DATABASE_URL")
-TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
-# SQLAlchemy 엔진 생성
-engine = create_engine(DATABASE_URL)
-test_engine = create_engine(TEST_DATABASE_URL)
-
-# 세션 생성
+# 엔진 및 세션 초기화
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
-Base = declarative_base()  # Base 클래스 생성
-TestBase = declarative_base()
+# 베이스 클래스 정의
+Base = declarative_base()
 
-#database에 대한 연결
-def get_db() -> Generator[Session, None, None]:
+# 의존성 주입용 함수 (DB 세션 관리)
+def get_db():
     db = SessionLocal()
     try:
         yield db
-
     finally:
         db.close()
-
-def get_test_db() -> Generator[Session, None, None]:
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# 테이블 생성
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-
-def init_test_db():
-    TestBase.metadata.create_all(bind=test_engine)
--- wellness/app/main.py
+++ wellness/app/main.py
@@ -8,68 +8,127 @@ from app.models import Total_Today, History
 from datetime import date, datetime
 from app.recommend import recommend_nutrition
 from app.database import get_db
-from decimal import Decimal, ROUND_HALF_UP
-from typing import List
-app = FastAPI(
-    docs_url="/docs",  # Swagger UI 비활성화
-    redoc_url="/redocs" 
-)
+from app.s3 import upload_image_to_s3
+from app.crud import get_food_by_category, get_recommend_by_user
+from PIL import Image, ExifTags
+import uuid
+import uvicorn
+from io import BytesIO
+import datetime
+import os
 
-Base.metadata.create_all(bind=engine)
 
 
+app = FastAPI()
 
-@app.get("/recommend/eaten_nutrient")
-def get_recommend_eaten(
-    user_id: int, 
-    date: str = Query(..., pattern=r"^\d{4}-\d{2}-\d{2}$"),
+def extract_exif_data(file_bytes: bytes):
+    """이미지에서 EXIF 데이터 추출하는 함수"""
+    try:
+        img = Image.open(BytesIO(file_bytes))
+        exif_data = img._getexif()
+        if not exif_data:
+            return None
+        
+        for tag, value in exif_data.items():
+            if ExifTags.TAGS.get(tag, tag) == "DateTimeOriginal":
+                return value  # 예: '2022:03:15 10:20:35'
+        return None
+    
+    except Exception as e:
+        logger.error(f"Error extracting Exif data: {str(e)}")
+        return None
+    
+        
+def determine_meal_type(taken_time: str) -> str:
+    """촬영 시간에 따른 meal_type 결정"""
+    try:
+        time_format = "%Y:%m:%d %H:%M:%S"
+        taken_time_obj = datetime.datetime.strptime(taken_time, time_format)
+        hour = taken_time_obj.hour
+
+        if 6 <= hour <= 8:
+            return "아침"
+        elif 11 <= hour <= 13:
+            return "점심"
+        elif 17 <= hour <= 19:
+            return "저녁"
+        else:
+            return "기타"
+    except Exception as e:
+        logger.error(f"Error determining meal type: {str(e)}")
+        return "기타"
+        
+@app.post("/api/v1/model/predict")
+async def classify_image(
+    file: UploadFile = File(...), 
+    user_id: int = Query(...),
     db: Session = Depends(get_db)
 ):
-     # 문자열 date를 datetime 객체로 변환
     try:
-         date_obj = datetime.strptime(date, "%Y-%m-%d").date()
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid date format. Please use YYYY-MM-DD.")
-    
-    # 사용자 정보 확인
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    
-    # 사용자가 없을 경우
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        # 메모리에서 파일 읽기
+        file_bytes = await file.read()
+        file_extension = file.filename.split(".")[-1]
+        unique_file_name = f"{uuid.uuid4()}.{file_extension}"
 
-     # 권장 영양소 정보 확인 및 업데이트   
-    try:
-        recommendation = crud.get_or_update_recommendation(db, user_id)
+        # .env 파일에서 bucket_name 가져오기
+        bucket_name =  os.getenv("BUCKET_NAME", "default_bucket_name")
+        
+        # 이미지 s3에 업로드
+        try:
+            image_url = upload_image_to_s3(BytesIO(file_bytes), bucket_name, unique_file_name)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"failed to upload image to s3: {str(e)}")
+        
+         # EXIF에서 촬영 날짜 추출
+        date = extract_exif_data(file_bytes)
+        meal_type = determine_meal_type(date) if date else "기타"
+
+        # 모델에 분류 요청 (model_api.py의 /predict_url/에 요청)
+        model_api_url = "http://localhost:8001/predict_url/"  # model_api 서버로 요청
+        try:
+            response = requests.post(model_api_url, params={"image_url": image_url})
+            response.raise_for_status
+        except requests.RequestException as e:
+            raise HTTPException(status_code=500, detail=f"Model API request failed: {str(e)}")
+
+        # 분류된 카테고리 ID 가져오기
+        category_id = response.json().get("category_id")
+        if category_id is None:
+            raise HTTPException(status_code=400, detail="Category ID is required")
+
+        # 분류된 결과로 음식 정보 조회
+        food = get_food_by_category(db, category_id)
+        if not food:
+            raise HTTPException(status_code=404, detail="Food not found")
+        
+        # user_id로 권장량 정보 조회
+        recommend = get_recommend_by_user(db, user_id)
+        if not recommend:
+            raise HTTPException(status_code=404, detail="user not found")
+
+        # 결과 반환
+        return {
+            "status": "success",
+            "date": date, 
+            "meal_type": meal_type, 
+            "category_id": category_id, 
+            "food_name": food.category_name, 
+            "food_kcal": food.food_kcal, 
+            "food_car": round(food.food_car), 
+            "food_prot": round(food.food_prot), 
+            "food_fat": round(food.food_fat), 
+            "rec_kcal": recommend.rec_kcal, 
+            "rec_car": round(recommend.rec_car), 
+            "rec_prot": round(recommend.rec_prot), 
+            "rec_fat": round(recommend.rec_fat),
+            "image_url": image_url
+            }
+    
     except HTTPException as e:
+        logger.error(f"HTTP error occurred: {str(e)}")
         raise e
-    
-    if recommendation is None:
-        raise HTTPException(status_code=500, detail="Failed to retrieve or create recommendations")
-    # 총 섭취량 조회 및 기본값 설정
-    try:
-        total_today = crud.get_or_create_total_today(db, user_id, date_obj)
-    except HTTPException as e:
-        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
-    # condition 업데이트
-    total_today.condition = total_today.total_kcal > recommendation.rec_kcal  
-    crud.update_total_today(db,total_today)
-    
-    return {
-    "status": "success",
-    "total_kcal": Decimal(total_today.total_kcal).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "total_car": Decimal(total_today.total_car).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "total_prot": Decimal(total_today.total_prot).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "total_fat": Decimal(total_today.total_fat).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "rec_kcal": Decimal(recommendation.rec_kcal).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "rec_car": Decimal(recommendation.rec_car).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "rec_prot": Decimal(recommendation.rec_prot).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "rec_fat": Decimal(recommendation.rec_fat).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP),
-    "condition": total_today.condition
-}
-
-
-
-
-
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="localhost", port=8000, reload=True)

6de11f14366becfd6c5465caee440170f15de0e1
haeunlee <gkdmsgkdms22@gmail.com>
2024-09-20 13:59:03 +0900

    git merge ready

--- Wellness/app/database.py
+++ Wellness/app/database.py
@@ -1,25 +1,49 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+from sqlalchemy.orm import Session, declarative_base
+from typing import Generator
 from dotenv import load_dotenv
 
-# .env 파일 로드(환경변수 설정)
+# .env 파일 로드
 load_dotenv()
 
-# postgresql 데이터베이스 url을 환경변수로부터 가져오기
+# 데이터베이스 URL 설정 
 DATABASE_URL = os.getenv("DATABASE_URL")
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
-# 엔진 및 세션 초기화
-engine = create_engine(DATABASE_URL, echo=True)
+# SQLAlchemy 엔진 생성
+engine = create_engine(DATABASE_URL)
+test_engine = create_engine(TEST_DATABASE_URL)
+
+# 세션 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
-# 베이스 클래스 정의
-Base = declarative_base()
+Base = declarative_base()  # Base 클래스 생성
+TestBase = declarative_base()
 
-# 의존성 주입용 함수 (DB 세션 관리)
-def get_db():
+#database에 대한 연결
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()
+
+def get_test_db() -> Generator[Session, None, None]:
+    db = TestSessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+# 테이블 생성
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def init_test_db():
+    TestBase.metadata.create_all(bind=test_engine)
